ae76c9a7be7543df24e2d408d0bb33535b890d47
John Rhoden <jrho19.jr@gmail.com>
2016-08-19 15:28:21 -0400

    Create action for API allows HTTP Post request

--- app/controllers/api/v1/patients_controller.rb
+++ app/controllers/api/v1/patients_controller.rb
@@ -1,7 +1,24 @@
 class Api::V1::PatientsController < ApplicationController
   respond_to? :json
+  skip_before_action :verify_authenticity_token
 
   def show
     render json: Patient.find(params[:id])
+  end
+
+  def create
+    patient = Patient.new(patient_params)
+
+    # if the patient is saved successfully than respond with json data and status code 201
+    if patient.save
+      render json: patient, status: 201
+    else
+      render json: { errors: patient.errors }, status: 422
+    end
+  end
+
+  private
+  def patient_params
+    params.require(:patient).permit(:start_time, :end_time, :first_name, :last_name, :comments)
   end
 end
